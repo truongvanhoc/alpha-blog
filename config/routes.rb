89ab738ca0cda3c44249b96e44edc81f3a4ea109
Truong Van Hoc <truongvanhoc@yahoo.com>
2016-07-01 09:43:04 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   
   resources :articles
   
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
